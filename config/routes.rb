ee178ed06863aed3c3cd7699294461e9e4899d6a
ccppjj1981 <macbook@macbookdeMacBook-Air.local>
2016-11-05 00:04:23 +0800

    提交评论功能”

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   #get 'articles/new'
   #get 'welcome/index'
   resources :articles
+  resources :articles do
+    resources :comments
+  end
   root 'welcome#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161102153941) do
+ActiveRecord::Schema.define(version: 20161104133405) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,4 +22,14 @@ ActiveRecord::Schema.define(version: 20161102153941) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "comments", force: :cascade do |t|
+    t.string   "commenter"
+    t.text     "body"
+    t.integer  "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_comments_on_article_id", using: :btree
+  end
+
+  add_foreign_key "comments", "articles"
 end
